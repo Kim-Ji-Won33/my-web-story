1e89672f0514649ca42ea6f26e558d13ae2317f8
Bambi0311 <71619548+Kim-Ji-Won33@users.noreply.github.com>
2022-05-09 00:01:53 +0900

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,11 +1,11 @@
 var Links ={
     setColor : function (color){
-//      var alist = document.querySelectorAll('a');
-//      var i = 0;
-//      while(i < alist.length){
-//        alist[i].style.color = color;
-//        i = i + 1;
-        $('a').css('color',color);
+     var alist = document.querySelectorAll('a');
+     var i = 0;
+     while(i < alist.length){
+       alist[i].style.color = color;
+       i = i + 1;
+//         $('a').css('color',color);
         
      }
     }
@@ -14,12 +14,12 @@ var Links ={
 
   var Body ={
     setColor : function (color){
-//      document.querySelector('body').style.color = color;
-        $('body').css('color',color);
+     document.querySelector('body').style.color = color;
+//         $('body').css('color',color);
         },
     setBackgroundColor : function (color){
-//      document.querySelector('body').style.backgroundColor = color;
-        $('body').css('backgroundColor',color);
+     document.querySelector('body').style.backgroundColor = color;
+//         $('body').css('backgroundColor',color);
     }
   }
 
